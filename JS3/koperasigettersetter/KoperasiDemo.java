cd6a0e90d0274d933536daf80a0eb4df9a114696
Salbila <salbiladj8@gmail.com>
2026-09-11 23:37:08 +0700

    Percobaan4_js3

--- JS3/koperasigettersetter/KoperasiDemo.java
+++ JS3/koperasigettersetter/KoperasiDemo.java
@@ -2,7 +2,7 @@ package JS3.koperasigettersetter;
 
 public class KoperasiDemo {
     public static void main(String[] args) {
-        Anggota anggota1 = new Anggota();
+        Anggota anggota1 = new Anggota("Iwan", "Jalan Mawar", 0);
         System.out.println("Simpanan " + anggota1.getNama() + " : Rp " + anggota1.getSimpanan());
         anggota1.setNama("Iwan Setiawan");
         anggota1.setAlamat("Jalan Sukarno Hatta no 10");
