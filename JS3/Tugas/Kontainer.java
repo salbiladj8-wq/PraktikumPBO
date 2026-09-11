dde345813a9d5663c67824336c51b6e2dab82aeb
Salbila <salbiladj8@gmail.com>
2026-09-12 00:29:28 +0700

    Tugas_PertanyaanNo4_js3

---
+++ JS3/Tugas/Kontainer.java
@@ -0,0 +1,47 @@
+package JS3.Tugas;
+
+public class Kontainer {
+    private String nomorResi;
+    private String namaPemilik;
+    private double kapasitasMaksimal;
+    private double beratMuatanSaatIni;
+
+    public Kontainer(String nomorResi, String namaPemilik, double kapasitasMaksimal) {
+        this.nomorResi = nomorResi;
+        this.namaPemilik = namaPemilik;
+        this.kapasitasMaksimal = kapasitasMaksimal;
+        this.beratMuatanSaatIni = 0;
+    }
+
+    public String getNomorResi() {
+        return nomorResi;
+    }
+
+    public String getNamaPemilik() {
+        return namaPemilik;
+    }
+
+    public double getKapasitasMaksimal() {
+        return kapasitasMaksimal;
+    }
+
+    public double getBeratMuatanSaatIni() {
+        return beratMuatanSaatIni;
+    }
+
+    public void tambahMuatan(double berat) {
+        if (this.beratMuatanSaatIni + berat > this.kapasitasMaksimal) {
+            System.out.println("Maaf, berat muatan melebihi kapasitas maksimal kontainer.");
+        } else {
+            this.beratMuatanSaatIni += berat;
+        }
+    }
+
+    public void turunkanMuatan(double berat) {
+        if (this.beratMuatanSaatIni - berat < 0) {
+            System.out.println("Maaf, muatan yang diturunkan melebihi muatan saat ini.");
+        } else {
+            this.beratMuatanSaatIni -= berat;
+        }
+    }
+}
---
+++ JS3/Tugas/TestLogistik.java
@@ -0,0 +1,26 @@
+package JS3.Tugas;
+
+public class TestLogistik {
+    public static void main(String[] args) {
+        Kontainer kontainerAlfa = new Kontainer("REQ-9988", "PT. Maju Bersama", 5000);
+
+        System.out.println("Nama Pemilik Kontainer: " + kontainerAlfa.getNamaPemilik());
+        System.out.println("Kapasitas Maksimal: " + kontainerAlfa.getKapasitasMaksimal() + " kg");
+
+        System.out.println("\nMemasukkan muatan baru seberat 6.000 kg...");
+        kontainerAlfa.tambahMuatan(6000);
+        System.out.println("Berat muatan saat ini: " + kontainerAlfa.getBeratMuatanSaatIni() + " kg");
+
+        System.out.println("\nMemasukkan muatan baru seberat 4.000 kg...");
+        kontainerAlfa.tambahMuatan(4000);
+        System.out.println("Berat muatan saat ini: " + kontainerAlfa.getBeratMuatanSaatIni() + " kg");
+
+        System.out.println("\nMembongkar muat/menurunkan barang seberat 500 kg...");
+        kontainerAlfa.turunkanMuatan(500);
+        System.out.println("Berat muatan saat ini: " + kontainerAlfa.getBeratMuatanSaatIni() + " kg");
+
+        System.out.println("\nMembongkar muat/menurunkan barang seberat 1.500 kg...");
+        kontainerAlfa.turunkanMuatan(1500);
+        System.out.println("Berat muatan saat ini: " + kontainerAlfa.getBeratMuatanSaatIni() + " kg");
+    }
+}
