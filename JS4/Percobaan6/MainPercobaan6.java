57c592f733fcfdedd526d7100f34deb212fe4d57
Salbila <salbiladj8@gmail.com>
2026-09-24 00:12:02 +0700

    Percobaan6_js6

---
+++ JS4/Percobaan6/MainPercobaan6.java
@@ -0,0 +1,9 @@
+package JS4.Percobaan6;
+
+public class MainPercobaan6 {
+    public static void main(String[] args) {
+        Laptop laptop = new Laptop("Thinkpad");
+        Printer printer = new Printer("Epson L3110");
+        laptop.cetakDokumen(printer, "Laporan.pdf");
+    }
+}
